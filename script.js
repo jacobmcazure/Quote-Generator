98154ae2eb85dda867efa71ed7474bca5909e02c
Jacob McEwen <jakemcewen7@gmail.com>
2024-07-12 21:18:42 -0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-console.log('test');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+function showLoadingDots() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoadingDots() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+function newQuote() {
+    showLoadingDots();
+    // pick a random apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.add('long-quote');
+    }
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    hideLoadingDots();
+}
+
+
+async function getQuotes() {
+    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+function tweetQuote() {
+    // template strings use backticks, not quotes
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// On Load
+getQuotes();
